a34dbdda9dc26d2fe34602ce10896918b40785b8
mengyounai <2193873302@qq.com>
2021-08-21 16:12:55 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<!--    <router-link to="/aaa">aaa</router-link>-->
+<!--    <router-link to="/hello">hello</router-link>-->
+<!--  <Login></Login>-->
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+  components: {
+
+  }
+});
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+// const root = document.getElementById('root')
+// root.textContent = '你妈贵姓？'
+import { createApp } from 'vue' // Vue 3.x 引入 vue 的形式
+import App from './App.vue' // 引入 APP 页面组建
+import router from "../route";
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+
+// @ts-ignore
+const app = createApp(App);
+app.use(router);
+app.use(Antd);
+app.mount('#app')
